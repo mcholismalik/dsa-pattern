49e1d483e8f3fde3ed5c1358923a002414635648
cholismalik <cholis.malik@shipper.id>
2023-10-29 23:25:22 +0700

    add creadme

---
+++ README.md
@@ -0,0 +1,15 @@
+# Data Structure and Algorithm in Golang
+
+This repository contains a collection of Golang code examples that demonstrate various data structures and algorithms commonly used in computer science and software development.
+
+## Topics Covered
+
+- Array and String Manipulation
+- Sliding Window
+- Two Pointers
+- Greedy Algorithms
+- Hashmap and Hashing
+- Breadth-First Search (BFS)
+- Depth-First Search (DFS)
+- Dynamic Programming
+- Backtracking
--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
-import "pg-go/pattern"
-
 func main() {
+	// array string
 	// sliding window
-	// substring
-	// backtracking
-	// greedy
-	// dynamic programming
 	// two pointers
-	// binary search
-	// graph
+	// greedy
+	// hashmap
+	// breadth first search
+	// depth first search
+	// dynamic programming
+	// backtracking
 
 	// array string
 	// as := pattern.InitArrayString()
@@ -21,37 +20,43 @@ func main() {
 	// as.RotateArray([]int{1, 2}, 5)
 	// as.RotateArray([]int{1, 2, 3}, 4)
 
-	// two pointers
-	// tp := pattern.InitTwoPointers()
-	// tp.TwoSum2([]int{2, 7, 11, 15}, 9)
-	// tp.TwoSum2([]int{2, 3, 4}, 6)
-
 	// sliding window
 	// sw := pattern.InitSlidingWindow()
 	// sw.LengthOfLongestSubstring("abcabcbb")
 	// sw.LengthOfLongestSubstring("abba")
+
+	// two pointers
+	// tp := pattern.InitTwoPointers()
+	// tp.TwoSum2([]int{2, 7, 11, 15}, 9)
+	// tp.TwoSum2([]int{2, 3, 4}, 6)
 
 	// greedy
 	// sw := pattern.InitGreedy()
 	// sw.JumpGame([]int{2, 3, 1, 1, 4})
 	// sw.JumpGame([]int{3, 2, 1, 0, 4})
 
-	// backtracking
-	bt := pattern.InitBacktracking()
-	// bt.Pemutations([]int{1, 2, 3})
-	bt.CombinationSum([]int{2, 3, 6, 7}, 7)
-	// bt.CombinationSum2([]int{2, 3, 6, 7}, 7)
-	// bt.CombinationSum([]int{2, 3, 5}, 8)
-
-	// dynamic programming
-	// dp := pattern.InitDynamicProgramming()
-	// dp.TriangleMinTotal([][]int{{2}, {3, 4}, {6, 5, 7}, {4, 1, 8, 3}})
-	// dp.TriangleMinTotal([][]int{{-10}})
-
 	// hashmap
 	// hm := pattern.InitHashmap()
 	// hm.GroupAnagram([]string{"eat", "tea", "tan", "ate", "nat", "bat"})
 	// hm.GroupAnagram([]string{""})
+
+	// breadth first search
+	// bfs := pattern.InitBreadthFirstSearch()
+	// bfs.BinaryTreeLevelOrderTraversal(&pattern.TreeNode{
+	// 	Val: 3,
+	// 	Left: &pattern.TreeNode{
+	// 		Val: 9,
+	// 	},
+	// 	Right: &pattern.TreeNode{
+	// 		Val: 20,
+	// 		Left: &pattern.TreeNode{
+	// 			Val: 15,
+	// 		},
+	// 		Right: &pattern.TreeNode{
+	// 			Val: 7,
+	// 		},
+	// 	},
+	// })
 
 	// depth first search
 	// dfs := pattern.InitDepthFirstSearch()
@@ -74,21 +79,15 @@ func main() {
 	// 	Val: -3,
 	// })
 
-	// breadth first search
-	// bfs := pattern.InitBreadthFirstSearch()
-	// bfs.BinaryTreeLevelOrderTraversal(&pattern.TreeNode{
-	// 	Val: 3,
-	// 	Left: &pattern.TreeNode{
-	// 		Val: 9,
-	// 	},
-	// 	Right: &pattern.TreeNode{
-	// 		Val: 20,
-	// 		Left: &pattern.TreeNode{
-	// 			Val: 15,
-	// 		},
-	// 		Right: &pattern.TreeNode{
-	// 			Val: 7,
-	// 		},
-	// 	},
-	// })
+	// dynamic programming
+	// dp := pattern.InitDynamicProgramming()
+	// dp.TriangleMinTotal([][]int{{2}, {3, 4}, {6, 5, 7}, {4, 1, 8, 3}})
+	// dp.TriangleMinTotal([][]int{{-10}})
+
+	// backtracking
+	// bt := pattern.InitBacktracking()
+	// bt.Pemutations([]int{1, 2, 3})
+	// bt.CombinationSum([]int{2, 3, 6, 7}, 7)
+	// bt.CombinationSum([]int{2, 3, 6, 7}, 7)
+	// bt.CombinationSum([]int{2, 3, 5}, 8)
 }
